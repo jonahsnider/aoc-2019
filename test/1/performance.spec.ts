7d3f3d25d5d1e49f3ca8bbd1f457d44c126a6ada
Jonah Snider <jonah@jonah.pw>
2019-12-02 20:53:21 -0800

    test: simplify test return values
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- test/1/performance.spec.ts
+++ test/1/performance.spec.ts
@@ -1,12 +1,8 @@
 import test from 'ava';
 import {part1, part2} from '../../src/1/performance';
 
-test('part 1 solution', t => {
-	// Confirmed to be correct from the answer page
-	return t.is(part1(), 3269199);
-});
+// Confirmed to be correct from the answer page
+test('part 1 solution', t => t.is(part1(), 3269199));
 
-test('part 2 solution', t => {
-	// Confirmed to be correct from the answer page
-	return t.is(part2(), 4900909);
-});
+// Confirmed to be correct from the answer page
+test('part 2 solution', t => t.is(part2(), 4900909));
